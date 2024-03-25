9d8ef1639b5d0fe9c54b4dfcf81f3317af301293
Knight-Thomas <thomasmilesknight@icloud.com>
2024-03-25 09:53:00 +0000

    adding button listeners

--- CalcApp.py
+++ CalcApp.py
@@ -8,8 +8,32 @@ class CalcUI(QtWidgets.QMainWindow):
         uic.loadUi('/Users/tomknight/GUI-CalculatorA/GUI-Calculator/calc_window.ui',self)
 
         #add button event listeners here
-
+        #self.Equalbutton.clicked.connect(self.btnInput())
+        #self.Clearbutton.clicked.connect(self.btnInput())
+        #self.Additionbutton.clicked.connect(self.btnInput())
+        #self.Subtractbutton.clicked.connect(self.btnInput())
+        #self.Multiplybutton.clicked.connect(self.btnInput())
+        #self.Divisionbutton.clicked.connect(self.btnInput())
+        #self.DecimaPoint.clicked.connect(self.btnInput())
+        self.Zerobutton.clicked.connect(lambda: self.btnInput('0'))
+        self.Onebutton.clicked.connect(lambda: self.btnInput('1'))
+        self.Twobutton.clicked.connect(lambda: self.btnInput('2'))
+        self.Threebutton.clicked.connect(lambda: self.btnInput('3'))
+        self.Fourbutton.clicked.connect(lambda: self.btnInput('4'))
+        self.Fivebutton.clicked.connect(lambda: self.btnInput('5'))
+        self.Sixbutton.clicked.connect(lambda: self.btnInput('6'))
+        self.Sevenbutton.clicked.connect(lambda: self.btnInput('7'))
+        self.Eightbutton.clicked.connect(lambda: self.btnInput('8'))
+        self.Ninebutton.clicked.connect(lambda: self.btnInput('9'))
+        
         self.show()
+
+    def btnInput(self, param):
+        '''handles a button click'''
+        self.current = self.current+str(param)
+        #self.Outputlabel.setText(self.current)
+        print(self.current)
+
     
 def mainApplication():
     app = QtWidgets.QApplication(sys.argv)
